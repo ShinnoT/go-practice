4d9ab001f3103f185c61cdbe43da88f0bf6898d1
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2018-05-06 00:21:53 +0900

    for loops in go

--- play-1.go
+++ play-1.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
+	printSomething()
+}
+
+// for loops
+
+func printSomething() {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("this is number %d\n", i)
+	}
 }
